b1f5b3c9329e6e86997ba4d52cfabb4cc9904e9c
DESKTOP-ENKK7LO\Roman <r.groiss18@gmail.com>
2018-05-18 17:42:07 +0200

    First Commit

---
+++ Bootstrap_toDoList_Clemens/Scripts/controller.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $("#addItemRow").click(function () {
+        $("#todoListTable").append(addDataRow());
+        $("#addItemRowTitle").val("");
+        $("#addItemRowContent").val("");
+        $(".removeItemRow").click(function () {
+            //this zeigt auf den Button removeItemRow, .parents("tr:first")[0].remove() löscht das erste Parent mit tr-Tag
+            $(this).parents("tr:first")[0].remove();
+        });
+    });
+});
+
---
+++ Bootstrap_toDoList_Clemens/Scripts/logic.js
@@ -0,0 +1,12 @@
+﻿counter = 0;
+
+function addDataRow() {
+    counter = counter + 1;
+    title = $("#addItemRowTitle").val();
+    content = $("#addItemRowContent").val();
+    checkbox = '<input type="checkbox" class="align - middle" style="margin - left: 15px" aria-label="Checkbox for following text input">';
+    button = '<button type="button" class="btn btn-secondary btn-outline-primary" id="addItemRow">Add</button>';
+    deleteBtn = '<button type="button" class="removeItemRow btn btn-secondary btn-outline-primary">Delete</button>';
+    return "<tr><td>" + title + "</td> <td>" + content + "</td> <td>" + checkbox + "</td> <td>" + button + deleteBtn + "</td></tr>";
+}
+
